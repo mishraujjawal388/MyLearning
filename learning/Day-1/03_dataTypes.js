1f8db748f4bdb7e2bf3d3714425ec73193d179f2
Ujjawal Mishra <79258995+mishraujjawal388@users.noreply.github.com>
2024-01-30 20:55:13 +0000

    operations done

--- learning/Day-1/03_dataTypes.js
+++ learning/Day-1/03_dataTypes.js
@@ -1,12 +1,12 @@
-2"use strict"; // treat all JS code as newer version
+// 2"use strict"; // treat all JS code as newer version
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+// console.log(3 
+//     +
+//      3) // code readability should be high
 
-console.log("Hitesh")
+// console.log("Hitesh")
 
 
 let name = "hitesh"
@@ -25,5 +25,38 @@ let state;
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// ****************************** Operations *************************************
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+1);
+// console.log(2-4);
+// console.log(2*5);
+// console.log(2/3);
+// console.log(2**4); // used double start for power
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Ujjawal"
+// console.log(str1+str2)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2 );
+// console.log(1+2+"2");
+
+// console.log( 3 + 4 * 5 % 3);
+// use paranthases write code in proper readable like
+// console.log( (3 + 4) * 4 / 4);
+
+console.log(+true);
+console.log(+ "");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
