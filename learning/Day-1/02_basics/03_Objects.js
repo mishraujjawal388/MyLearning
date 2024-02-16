a55c1912e5e09d2ae75de596266fa26dc44d0d0d
Ujjawal Mishra <79258995+mishraujjawal388@users.noreply.github.com>
2024-02-16 08:47:32 +0000

    object

--- learning/Day-1/02_basics/03_Objects.js
+++ learning/Day-1/02_basics/03_Objects.js
@@ -5,16 +5,42 @@
 
 
 // Objects literals
-// const mysymbol = symbol("key1");
+const mysymbol = Symbol("key1");
 const JsUser = {
     name: "ujjawal", //by default system takes name as string
     "fullName": "Ujjawal Mishra",
+    [mysymbol]: "mykey1", // you can use symbols in square bracket and pass value
     age: 18,
     email: "ujjawal@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Wednesday"],
 }
 
-console.log(JsUser.email)
-console.log(["email"])
-console.log(["email"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["fullName"])
+// console.log(JsUser[mysymbol]) // you can get symbol value using square bracket 
+// console.table([JsUser.email, JsUser["email"],JsUser["fullName"],JsUser[mysymbol]])
+
+JsUser.email = "ujjawal@mishra.com" //you can use = to overwrite value
+// console.table([JsUser.email, JsUser["email"],JsUser["fullName"],JsUser[mysymbol]])
+
+// you want to lock the value (not changeable) then you use Object.freeze methode
+
+Object.freeze(JsUser)
+JsUser.email = "ujjawal@tech.in" //its not printed or changed due to freeze property
+// console.table([JsUser.email, JsUser["email"],JsUser["fullName"],JsUser[mysymbol]])
+
+console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+// always use .(dot) methode to access value from objects
